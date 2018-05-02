1b4bbd89e2ba37f386bdd5afdb21ec1c73b8fca1
jn50 <Jack.C.Ni@newegg.com>
2018-05-02 11:49:08 +0800

    update

--- spring-web-base/src/main/java/com/newegg/ec/base/WebBase.java
+++ spring-web-base/src/main/java/com/newegg/ec/base/WebBase.java
@@ -1,11 +1,11 @@
 package com.newegg.ec.base;
 
 import com.newegg.ec.base.filter.AuthenticationFIlter;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+
 import javax.servlet.Filter;
 
 
@@ -13,18 +13,18 @@ import javax.servlet.Filter;
  * Created by jn50 on 2018/1/27.
  */
 @SpringBootApplication
-@MapperScan("com.newegg.ec.base.dao.mysql")
+@EnableWebBase
 public class WebBase {
 
     public static void main(String[] args) {
         SpringApplication.run(WebBase.class, args);
     }
 
-    /*@Bean
+    @Bean
     public FilterRegistrationBean someFilterRegistration() {
         FilterRegistrationBean registration = new FilterRegistrationBean();
         registration.setFilter(authenticationFilter());
-        registration.addUrlPatterns("/rest*//*");
+        registration.addUrlPatterns("/rest/*");
         registration.addInitParameter("paramName", "paramValue");
         registration.setName("sessionFilter");
         return registration;
@@ -33,5 +33,5 @@ public class WebBase {
     @Bean(name = "sessionFilter")
     public Filter authenticationFilter() {
         return new AuthenticationFIlter();
-    }*/
+    }
 }
